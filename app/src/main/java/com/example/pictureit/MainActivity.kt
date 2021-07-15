ade5bb8d27b2768c7a8ad148451e5e5ea589beb7
RaphaelNdonga <nkibi53@gmail.com>
2021-07-15 14:50:09 +0300

    The new method for obtaining activity results. From the gallery and camera

--- app/src/main/java/com/example/pictureit/MainActivity.kt
+++ app/src/main/java/com/example/pictureit/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.example.pictureit
 
-import android.content.ActivityNotFoundException
+import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
-import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.pictureit.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,47 +15,29 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        binding.cameraButton.setOnClickListener {
-            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            try {
-                startActivityForResult(intent, CAMERA_KEY)
-            } catch (exception: ActivityNotFoundException) {
-                Toast.makeText(
-                    this, "Your phone camera has an issue", Toast.LENGTH_LONG
-                ).show()
-            }
-        }
-        binding.galleryButton.setOnClickListener {
-            val intent = Intent(Intent.ACTION_GET_CONTENT)
-            intent.type = "image/*"
-            try {
-                startActivityForResult(intent, GALLERY_KEY)
-            } catch (exception: ActivityNotFoundException) {
-                Toast.makeText(
-                    this, "Your gallery apps have an issue", Toast.LENGTH_LONG
-                ).show()
-            }
-        }
-        setContentView(binding.root)
-    }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == CAMERA_KEY && resultCode == RESULT_OK) {
-            val imageBitmap = data?.extras?.get("data") as Bitmap
-            binding.cameraImage.setImageBitmap(imageBitmap)
-        }
-        if (requestCode == GALLERY_KEY && resultCode == RESULT_OK) {
-            val imageUri = data?.data
-            imageUri?.let {
-                val imageStream = contentResolver.openInputStream(imageUri)
-
-                /**
-                 * Content resolvers allow us to access the content providers of other applications.
-                 */
+        val getGalleryPicture =
+            registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+                val imageStream = contentResolver.openInputStream(uri)
                 val selectedImage = BitmapFactory.decodeStream(imageStream)
                 binding.galleryImage.setImageBitmap(selectedImage)
             }
+
+        val getCameraPicture =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    val bitmap = result.data?.extras?.get("data") as Bitmap
+                    binding.cameraImage.setImageBitmap(bitmap)
+                }
+            }
+
+        binding.cameraButton.setOnClickListener {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            getCameraPicture.launch(intent)
         }
+        binding.galleryButton.setOnClickListener {
+            getGalleryPicture.launch("image/*")
+        }
+        setContentView(binding.root)
     }
 }
